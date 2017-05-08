4fbd511e395b056daefd38e5fe3b3072ef9a0c88
Moses Samuel <mosamuel98@gmail.com>
2017-05-08 10:24:29 -0700

    remove superfluous client code + node dependencies

--- client/scripts/auth.js
+++
@@ -1,4 +0,0 @@
-/* OAUTH2 client scripts will go here. 
- I should have a basic prototype up and running pretty soon -- hopefully by Monday morning :)
--- Moses A. Samuel
-*/ 
--- client/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-render( < App / > , document.getElementById('root'));
-
-console.log('Hello Moses');
